1eee65021d74ff9e53609b2ea3f2e9802af66c8c
Matthew Dang <mattdbruin@gmail.com>
2017-06-21 18:48:44 -0700

    Greet multiple people

--- index.android.js
+++ index.android.js
@@ -1,33 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
 
- import React, { Component } from 'react';
- import { AppRegistry, Image, View } from 'react-native';
- class Picture extends Component {
-   render() {
-     return (
-       <Image source={this.props.pic} style={{width: 400, height: 200}}/>
-     );
-   }
- }
- export default class Pictures extends Component {
-   render() {
-     let pic1 = {
-       uri: 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/19145884_10211228744652465_1508890654774567987_n.jpg?oh=2cee0d6d955e10191e4351c13cb9fa4e&oe=59DE8669'
-     };
-     let pic2 = {
-       uri: 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/18897_10205429954486335_5410535146248325801_n.jpg?oh=e021b8d7ff66cad6fe080ee571466385&oe=59DAB977'
-     };
-     return (
-             <View style={{alignItems: 'center'}}>
-               <Picture pic={pic1} />
-               <Picture pic={pic2} />
-             </View>
-           );
-   }
- }
- // skip this line if using Create React Native App
- AppRegistry.registerComponent('AwesomeProject', () => Pictures );
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Melissa Leong' />
+        <Greeting name='Kevin Bao' />
+        <Greeting name='Tracey Chan' />
+        <Greeting name='Justin Leong' />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
