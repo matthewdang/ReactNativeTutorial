1f324466ad1a27bc60cb446986d4defba3406ecd
Matthew Dang <mattdbruin@gmail.com>
2017-06-21 18:15:18 -0700

    Add 2 images

--- index.android.js
+++ index.android.js
@@ -4,13 +4,30 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
-export default class HelloWorldApp extends Component {
-  render () {
-    return (
-        <Text>Justin Leong Loves Tracey Chan</Text>
-    );
-  }
-}
-AppRegistry.registerComponent('AwesomeProject', () =>HelloWorldApp);
+ import React, { Component } from 'react';
+ import { AppRegistry, Image, View } from 'react-native';
+ class Picture extends Component {
+   render() {
+     return (
+       <Image source={this.props.pic} style={{width: 400, height: 200}}/>
+     );
+   }
+ }
+ export default class Pictures extends Component {
+   render() {
+     let pic1 = {
+       uri: 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/19145884_10211228744652465_1508890654774567987_n.jpg?oh=2cee0d6d955e10191e4351c13cb9fa4e&oe=59DE8669'
+     };
+     let pic2 = {
+       uri: 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/18897_10205429954486335_5410535146248325801_n.jpg?oh=e021b8d7ff66cad6fe080ee571466385&oe=59DAB977'
+     };
+     return (
+             <View style={{alignItems: 'center'}}>
+               <Picture pic={pic1} />
+               <Picture pic={pic2} />
+             </View>
+           );
+   }
+ }
+ // skip this line if using Create React Native App
+ AppRegistry.registerComponent('AwesomeProject', () => Pictures );
