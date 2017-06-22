df192d67728ad524a3d8c90a7d5e4390a5ea1d47
Matthew Dang <mattdbruin@gmail.com>
2017-06-22 13:36:20 -0700

    Make blinking text

--- index.android.js
+++ index.android.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, View } from 'react-native';
+import { AppRegistry, Text, View } from 'react-native';
 
-class Picture extends Component {
-  render() {
-      let pic = {
-        uri: this.props.name
-      };
-      return (
-        <Image source={pic} style={{width: 400, height: 200}}/>
-      );
+class Blink extends Component{
+  constructor(props) {
+    super(props) ;
+    this.state = { showText: true } ;
+
+    // Toggle the state of every second
+    setInterval (() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText } ;
+      });
+    }, 1000) ;
   }
-}
 
-export default class LotsofPictures extends Component {
   render() {
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-      <View style={{alignItems:'center'}}>
-        <Picture name='https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/10613148_1244376775587972_376261394166346151_n.jpg?oh=a2b9df10e01874648fb5c8c9aaeaac9a&oe=59C847F0'/>
-        <Picture name='https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/19145884_10211228744652465_1508890654774567987_n.jpg?oh=2cee0d6d955e10191e4351c13cb9fa4e&oe=59DE8669'/>
-      </View>
-    );
+      <Text>{display}</Text>
+    ) ;
   }
 }
 
-AppRegistry.registerComponent('AwesomeProject', () => LotsofPictures);
+export default class BlinkApp extends Component {
+  render () {
+    return (
+      <View>
+        <Blink text='I love to blink' />
+        <Blink text='Yes bllinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+      </View>
+    ) ;
+  }
+}//skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
